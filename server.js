f6141dc3f786526efd07b22c6c741ba120e4f5a9
stkwn <34761@qq.com>
2026-05-02 14:22:39 +1000

    update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
@@ -7,8 +8,6 @@ import dreamsRouter from './routes/dreams.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-
 const app = express();
 
 /*
